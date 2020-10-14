a1e24a5a922d49a08d4cb53f0c0c9428568f1acd
han <15162294433@163.com>
2020-10-14 18:08:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<Button></Button>
+</template>
+
+<script>
+import {
+  Button
+} from 'perry-ui'
+export default {
+  name: 'App',
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style>
+@import 'perry-ui/dist/lib/perry.css';
+</style>
